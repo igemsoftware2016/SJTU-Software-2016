64576b5a225c782443fd39c2b3a73de8ff2e73ec
JieYao <yj950714@163.com>
2016-10-13 23:49:33 +0800

    change origin branch

--- website/base/DeviceDesign/advice/judge.php
+++
@@ -1,6 +0,0 @@
-<?php
-	$type = $_POST['usertype'];
-	$id = $_POST['userid'];
-	exec("perl judge.pl $id $type", $out);
-	echo $out[0]
-?>
--- website/imap2/front-end/mainJS.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function(){
-	var width = $("#helpModel").width()
-	$("#helpModel").css("margin-left", -1 * (width) / 2)
-	//set the position of help view
-})
