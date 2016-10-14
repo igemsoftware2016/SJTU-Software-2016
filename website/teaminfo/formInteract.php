e2c934d5d6cf484f2462c1f3249be67d405276c1
JieYao <yj950714@163.com>
2016-10-14 18:16:28 +0800

    Change Some files\' position and some href address in the html files

--- website/teaminfo/formInteract.php
+++ website/teaminfo/formInteract.php
@@ -6,7 +6,7 @@ if ($db->connect_errno>0)
 	echo "no1";
     die("Unable to connect to database [".$db->connect_error."]");
 }
-$sql="select * from Teams";
+$sql="select * from Teams order by Team_Name";
 if (!$result=$db->query($sql))
 {
 	echo "no2";
